51597bef64350ae6d73d4babcb377df889087cdf
piyush99755 <60968737+piyush99755@users.noreply.github.com>
2026-02-15 11:05:13 -0500

    Install Auth dependencies and create user blueprint in models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy import Column, Integer, String, Text, DateTime, Boolean
 from sqlalchemy.sql import func
 from .database import Base
 
@@ -12,3 +12,13 @@ class NailArt(Base):
     image_url = Column(String(255), nullable=False)
     created_at = Column(DateTime(timezone= True), server_default=func.now())
      
+class User(Base):
+    
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key = True, index = True)
+    email = Column(String(255), unique= True, index = True, nullable = False)
+    password_hash = Column(String(255), nullable= False)
+    is_admin = Column(Boolean, default = True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    
