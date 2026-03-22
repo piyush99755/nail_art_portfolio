5bef491536e2bd8273ed7b44ee5a309084d2084a
piyush99755 <60968737+piyush99755@users.noreply.github.com>
2026-03-22 15:57:48 -0400

    (frontend):Fix env file and axios config

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'; //import axios library
 
 //a pre-configured HTTP  client
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 //interceptors centralizes authentication logic 
